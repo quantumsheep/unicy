d90074e30491fa9a0fd666c3fe14918657167f0f
quantumsheep <nathanael.dmc@outlook.fr>
2020-01-16 10:03:25 +0100

    Add Mathf constants

--- include/unicy/Mathf.hpp
+++ include/unicy/Mathf.hpp
@@ -1,9 +1,20 @@
 #pragma once
 
 #include <cmath>
+#include <limits>
 
 namespace Unicy::Mathf
 {
+
+constexpr float PI = 3.14159274f;
+
+constexpr float Infinity = std::numeric_limits<float>::infinity();
+constexpr float NegativeInfinity = -Infinity;
+
+constexpr float Deg2Rad = 0.0174532924f;
+constexpr float Rad2Deg = 57.29578f;
+
+constexpr float Epsilon = std::numeric_limits<float>::epsilon();
 
 const auto Sqrt = std::sqrtf;
 
