4271f2b5e3e1ec396c6ce21cc26a115f183e3476
quantumsheep <nathanael.dmc@outlook.fr>
2020-01-18 16:40:22 +0100

    Quaternion: fix components order and add Magnitude function

--- include/unicy/Quaternion.hpp
+++ include/unicy/Quaternion.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <unicy/Mathf.hpp>
 #include <unicy/Vector3.hpp>
 
 #include <string>
@@ -10,23 +11,30 @@ namespace Unicy
 class Quaternion
 {
 public:
+    float w;
     float x;
     float y;
     float z;
-    float w;
 
     Quaternion() = default;
     Quaternion(const Quaternion &) = default;
     Quaternion(Quaternion &&) = default;
 
-    Quaternion(const float &x, const float &y, const float &z, const float &w) : x(x), y(y), z(z), w(w) {}
+    Quaternion(const float &w, const float &x, const float &y, const float &z) : w(w), x(x), y(y), z(z) {}
 
-    static Quaternion Identity() { return Quaternion(0, 0, 0, 1); }
+    static Quaternion Identity() { return Quaternion(1, 0, 0, 0); }
 
     std::string to_string() const;
 
     Vector3 eulerAngles() const;
     void eulerAngles(const Vector3 &angles);
+
+    static inline float Magnitude(const Quaternion &q)
+    {
+        return Mathf::Sqrt((q.w * q.w) + (q.x * q.x) + (q.y * q.y) + (q.z * q.z));
+    }
+
+    inline float magnitude() { return Quaternion::Magnitude(*this); }
 
     friend inline std::ostream &operator<<(std::ostream &os, const Quaternion &r)
     {
