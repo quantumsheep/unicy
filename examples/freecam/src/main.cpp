9477dcdfcf3974531969991b9bd3cf97ef33dfe8
QuantumSheep <quantumsheep@outlook.fr>
2020-01-20 17:03:28 +0100

    Refresh freecam example with current possibilities

--- examples/freecam/src/main.cpp
+++ examples/freecam/src/main.cpp
@@ -4,17 +4,35 @@ using namespace Unicy;
 
 int main()
 {
-    Vector3Int a(9, 3, 70);
+    auto window = Window(1280, 720, "Freecam Example");
 
-    Debug::Log(a);
-    // Debug::Log(Vector3::Magnitude(a));
-    // Debug::Log(Vector3::Normalize(a));
-    Debug::Log(a.normalized());
-    // Debug::Log(Vector3Int::positiveInfinity());
-    // Debug::Log(Vector3::negativeInfinity());
-    Debug::Log(a[0]);
-    Debug::Log(a[1]);
-    Debug::Log(a[2]);
+    if (window.error != EnvironmentError::None)
+    {
+        Debug::Log(stderr, "An error occured: %d\n", window.error);
+        return 1;
+    }
+
+    while (window.next_frame())
+    {
+        UI::NewFrame();
+
+        UI::Window("Debug", [&] {
+            UI::Separator();
+
+            static bool wireframe = false;
+            UI::Checkbox("Wireframe", &wireframe);
+
+            ImGui::ColorEdit4("Background", window.background);
+
+            glPolygonMode(GL_FRONT_AND_BACK, wireframe ? GL_LINE : GL_FILL);
+        });
+
+        UI::Render();
+        UI::Draw();
+
+        window.update_viewport();
+        window.swap_buffers();
+    }
 
     return 0;
 }
